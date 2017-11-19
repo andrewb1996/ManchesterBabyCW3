937f6a08c6cc75fc7e12ef4ac0142bc275321f20
Peter <pkwjn1633@gmail.com>
2017-11-19 22:33:16 +0000

    Fixed some compile errors

--- Assembler.cpp
+++ Assembler.cpp
@@ -15,6 +15,9 @@
 #include <string>
 #include <bitset>
 
+//For opening simulator
+#include <sstream>
+
 using namespace std;
 
 string codeFile[256];
@@ -230,8 +233,9 @@ string compileCode(int variableNumber)
 			//for both decinal values, convert to binary using the convertDecToBinary function
 			//for the third cgange the value of the for statement to a string
 			variableData[variableNumber][1] = convertDecToBinary(value,"variable");
-			variableData[variableNumber][2] = convertDecToBinary(i,"operand");				
-			variableData[variableNumber][3] = to_string(i);
+			variableData[variableNumber][2] = convertDecToBinary(i,"operand");	
+			string I.to_string(i);
+			variableData[variableNumber][3] = I;
 			variableNumber++;
 		}
 		
@@ -355,7 +359,8 @@ string scanCode()
 		{
 			//display an error message to the user stating which line and what the problem is.
 			errorString = "ERROR: Line ";
-			errorString += to_string(i);
+			string errorLine.to_string(i);
+			errorString += errorLine;
 			errorString += ". There is a problem with opcode: ";
 			errorString += placeholder;
 			return errorString;
@@ -407,7 +412,8 @@ string scanCode()
 						if (foundAt != string::npos) 
 						{
 							//display an error message to the user stating which line and what the problem is.
-							errorString = "ERROR: Line " + std::to_string(i) + ". There is a problem with the variable number; " + codeFile[i].substr(positionFound,32);
+							string errorLine.to_string(i);
+							errorString = "ERROR: Line " + errorLine + ". There is a problem with the variable number; " + codeFile[i].substr(positionFound,32);
 							return errorString;
 						}
 					}
